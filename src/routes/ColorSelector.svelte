d4d67819013d0a74236aea7dfe99b41303e543f9
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2023-12-06 19:50:28 +0100

    added scuffed custom colors

--- src/routes/ColorSelector.svelte
+++ src/routes/ColorSelector.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import ColorSelectorPixel from "./ColorSelectorPixel.svelte";
+    import {blue, green, red} from "./store";
 
     let colors = [
         [0,0,0],
@@ -18,11 +19,25 @@
         [255,0,127],
     ];
 
+    let customVisible = false;
+
 </script>
 
 <div class="absolute left-0 top-1/2 -translate-y-1/2">
-{#each colors as color}
-    <ColorSelectorPixel bind:r={color[0]} bind:g={color[1]} bind:b={color[2]} size={15} />
-{/each}
-
+    {#each colors as color}
+        <ColorSelectorPixel bind:r={color[0]} bind:g={color[1]} bind:b={color[2]} size={15} />
+    {/each}
+    <div class="flex">
+        <div class="w-[15px] h-[15px] border-[2px] border-black flex items-center justify-center" on:click={() => customVisible = !customVisible} role="none">
+            <i class="fa-solid fa-plus scale-[70%] color"></i>
+        </div>
+    </div>
+    {#if customVisible}
+        <div class="w-max rounded h-max bg-gray-600 bg-opacity-90 flex items-center justify-center p-4 flex-col">
+            <input class="mb-1 border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent rounded p-1" bind:value={$red}>
+            <input class="mb-1 border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent rounded p-1" bind:value={$green}>
+            <input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent rounded p-1" bind:value={$blue}>
+        </div>
+    {/if}
 </div>
+
