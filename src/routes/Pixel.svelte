d602dfb6dd2a3d9c01b5823ebf2660f3941a2efd
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2023-12-07 11:17:01 +0000

    added dragging

--- src/routes/Pixel.svelte
+++ src/routes/Pixel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 
-    import {blue, green, red} from "./store";
+    import {blue, dragging, green, red} from "./store";
 
     export let r: number;
     export let g: number;
@@ -9,6 +9,19 @@
 </script>
 
 <div class="border-none caret-transparent p-0 m-0 cursor-crosshair" style="height: {size}px; width: {size}px; background-color: rgb({r}, {g}, {b});"
-     on:click="{() => {r=$red; g=$green; b=$blue}}"
-     role="img">
+        on:click="{() => {r=$red; g=$green; b=$blue}}"
+        on:mousedown={(event) => {
+            r=$red; g=$green; b=$blue
+            $dragging = true;
+            event.preventDefault();
+        }}
+        on:mouseup={() => {
+            $dragging = false;
+        }}
+        on:mouseenter={() => {
+            if($dragging) {
+                r=$red; g=$green; b=$blue;
+            }
+        }}
+     role="control">
 </div>
--- src/routes/store.ts
+++ src/routes/store.ts
@@ -4,3 +4,4 @@ import { writable } from 'svelte/store';
 export const red = writable(0);
 export const green = writable(0);
 export const blue = writable(0);
+export const dragging = writable(false);
