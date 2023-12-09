01f927c10647341c2547402e19fc3882b813d121
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2023-12-09 18:19:29 +0100

    removed annoying warnings

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	onwarn: (warning, handler) => {
+		if (warning.code === 'a11y-click-events-have-key-events'
+		|| warning.code === 'a11y-no-noninteractive-element-interactions'
+		|| warning.code === "a11y-no-static-element-interactions") return
+		handler(warning)
+	},
 };
 
 export default config;
